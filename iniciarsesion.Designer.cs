1d3998c0f2c974ae2acd2b5be2623b8df10195d6
Juan Sebastian Pérez Millares <89596142+JUANPER-GITHUB-2021@users.noreply.github.com>
2021-09-06 18:28:08 -0500

    Primer formulario (solo visual, sin codigo)

--- iniciarsesion.Designer.cs
+++ iniciarsesion.Designer.cs
@@ -63,7 +63,7 @@ namespace OnlyPans
             // Btniniciar
             // 
             this.Btniniciar.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.Btniniciar.Location = new System.Drawing.Point(297, 292);
+            this.Btniniciar.Location = new System.Drawing.Point(294, 283);
             this.Btniniciar.Name = "Btniniciar";
             this.Btniniciar.Size = new System.Drawing.Size(121, 26);
             this.Btniniciar.TabIndex = 2;
